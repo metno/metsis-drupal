efe20fabbfb53b66dc583730671faf1d5cbe4004
nemo <bards@met.no>
2018-10-11 14:02:47 +0200

    cleaned

--- drupal/2018-10-11-nbs-metsis/metsis_qsearch/theme/metadata.tpl.php
+++ drupal/2018-10-11-nbs-metsis/metsis_qsearch/theme/metadata.tpl.php
@@ -1,5 +1,5 @@
 <?php ?>
 
 <h2><?php print t('Available Metadata'); ?></h2>
-<?php print($metadata_table)?>
-<div> <a href="#" class="adc-button adc-back"><?php print t('Back to results')?></a></div>
+<?php print($metadata_table) ?>
+<div> <a href="#" class="adc-button adc-back"><?php print t('Back to results') ?></a></div>
