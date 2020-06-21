1ca91d46fcbb1419183c6a0354e2136486e0b63b
ferrighi <laraf@met.no>
2020-06-21 20:43:51 +0100

    temporary commit

--- metsis/metsis_qsearch/js/metsis_search_map.js
+++ metsis/metsis_qsearch/js/metsis_search_map.js
@@ -2,7 +2,7 @@
 var lon = Drupal.settings.lon;
 var lat = Drupal.settings.lat;
 var defzoom = Drupal.settings.zoom;
-init_proj = 'EPSG:4326';
+var init_proj = 'EPSG:4326';
 
 // 32661
 proj4.defs('EPSG:32661', '+proj=stere +lat_0=90 +lat_ts=90 +lon_0=0 +k=0.994 +x_0=2000000 +y_0=2000000 +datum=WGS84 +units=m +no_defs');
@@ -26,7 +26,7 @@ var proj32761 = new ol.proj.Projection({
 
 
 // 4326
-var ext4326 = [-180.0000, -90.0000, 180.0000, 90.0000]; 
+var ext4326 = [-350.0000, -90.0000, 350.0000, 90.0000]; 
 var center4326 = [15,70]; 
 var proj4326 = new ol.proj.Projection({
   code: 'EPSG:4326',
@@ -42,11 +42,19 @@ projObjectforCode = {
 
 var ch = document.getElementsByName('map-search-projection');
 
-document.getElementById('4326').checked = true;
+document.getElementById(init_proj).checked = true;
 
 for (var i = ch.length; i--;) {
    ch[i].onchange = function change_projection() {
       var prj = this.value;
+      if (map.getLayers().getArray().lenght !== 1){
+         if (map.getLayers().getArray().length > 2) {
+            map.getLayers().removeAt(2);
+            map.getLayers().removeAt(1,layer['draw']);
+         }else{
+            map.getLayers().removeAt(1,layer['draw']);
+         }
+      }
       if (prj == 'EPSG:32761') {
         map.getLayers().removeAt(0,layer['baseN']);
         map.getLayers().insertAt(0,layer['baseS']);
@@ -64,6 +72,10 @@ for (var i = ch.length; i--;) {
 
       layer['baseN'].getSource().refresh();
       layer['baseS'].getSource().refresh();
+      layer['europaveg'].getSource().refresh();
+      layer['fylkesveg'].getSource().refresh();
+      build_draw(projObjectforCode[prj].projection);
+      addExtraLayers(prj);
       }
 }
 
@@ -115,80 +127,177 @@ var mousePositionControl = new ol.control.MousePosition({
 });
 map.addControl(mousePositionControl);
 
-// Add drawing vector source
-var drawingSource = new ol.source.Vector({
-    projection: 'EPSG:4326'
-});
-//Add drawing layer
-var drawingLayer = new ol.layer.Vector({
-    source: drawingSource,
-    title: 'draw',
-    projection: 'EPSG:4326',
-});
-map.addLayer(drawingLayer);
 
-var draw; // global so we can remove it later
-draw = new ol.interaction.Draw({
-   source: drawingSource,
-   type: 'Circle',
-   geometryFunction: ol.interaction.Draw.createBox(),
-   maxPoints: 2
-});
+function build_draw(proj) {
 
+   // Add drawing vector source
+   var drawingSource = new ol.source.Vector({
+      projection: proj
+   });
+   //Add drawing layer
+   var drawingLayer = new ol.layer.Vector({
+      source: drawingSource,
+      title: 'draw',
+      projection: proj
+   });
+   map.addLayer(drawingLayer);
 
-var tllat = document.getElementById('edit-bbox-top-left-lat');
-var tllon = document.getElementById('edit-bbox-top-left-lon');
-var brlat = document.getElementById('edit-bbox-bottom-right-lat');
-var brlon = document.getElementById('edit-bbox-bottom-right-lon');
-
-draw.on('drawstart', function (e) {
-   drawingSource.clear();
-});
-
-draw.on('drawend', function (e) {
- 
-   var extent = e.feature.getGeometry().getExtent();
-   console.log(extent);
-   if (extent && extent.length === 4) {
-      extent = ol.proj.transformExtent(extent, map.getView().getProjection().getCode(), 'EPSG:4326');
-      var topLeft = ol.extent.getTopLeft(extent);
-      var bottomRight = ol.extent.getBottomRight(extent);
+   var geometryFunction = function (coordinates, geometry, projection) {
+      var start = coordinates[0]; //x,y
+      var end = coordinates[1];
+      
+      // transform in latlon
+      var start_ll = ol.proj.transform(start, projection, 'EPSG:4326'); //lon,lat
+      var end_ll = ol.proj.transform(end, projection, 'EPSG:4326');
+      var left_ll = [start_ll[0], end_ll[1]]; 
+      var right_ll = [end_ll[0], start_ll[1]]; 
+      
+      var left = ol.proj.transform(left_ll,'EPSG:4326', projection);
+      var right = ol.proj.transform(right_ll, 'EPSG:4326', projection);
+      
+      
+      const boxCoordinates = [
+        [
+          start, left, end, right, start,
+        ],
+      ];
+      
+      if (geometry) {
+         geometry.setCoordinates(boxCoordinates);
+      } else {
+         geometry = new ol.geom.Polygon(boxCoordinates);
+      }
+      return geometry;
    }
+   var draw; // global so we can remove it later
+   draw = new ol.interaction.Draw({
+      source: drawingSource,
+      type: 'LineString',
+      //geometryFunction: ol.interaction.Draw.createBox(),
+      geometryFunction: geometryFunction,
+      maxPoints: 2
+   });
 
-   jQuery(tllat).attr('value', topLeft[1]);
-   jQuery(tllon).attr('value', topLeft[0]);
-   jQuery(brlat).attr('value', bottomRight[1]);
-   jQuery(brlon).attr('value', bottomRight[0]);
 
+   var tllat = document.getElementById('edit-bbox-top-left-lat');
+   var tllon = document.getElementById('edit-bbox-top-left-lon');
+   var brlat = document.getElementById('edit-bbox-bottom-right-lat');
+   var brlon = document.getElementById('edit-bbox-bottom-right-lon');
 
-    console.log('end');
-});
+   draw.on('drawstart', function (e) {
+      drawingSource.clear();
+   });
 
-map.addInteraction(draw);
+   draw.on('drawend', function (e) {
+    
+      coords = e.feature.getGeometry().getCoordinates();
+      var a = ol.proj.transform(coords[0][0], map.getView().getProjection().getCode(), 'EPSG:4326');
+      var b = ol.proj.transform(coords[0][1], map.getView().getProjection().getCode(), 'EPSG:4326');
+      var c = ol.proj.transform(coords[0][2], map.getView().getProjection().getCode(), 'EPSG:4326');
+      var d = ol.proj.transform(coords[0][3], map.getView().getProjection().getCode(), 'EPSG:4326');
+      var e = ol.proj.transform(coords[0][4], map.getView().getProjection().getCode(), 'EPSG:4326');
+      var topLeft = [Math.min(a[0],c[0]), Math.max(a[1],c[1])];
+      var bottomRight = [Math.max(a[0],c[0]), Math.min(a[1],c[1])];
+   
+//    if (topLeft[0]<-180){
+//       topLeft[0] += 360;
+//    } else if (topLeft[0]>180){
+//       topLeft[0] -= 360;
+//    }
+//    if (bottomRight[0]<-180){
+//       bottomRight[0] += 360;
+//    } else if (bottomRight[0]>180){
+//       bottomRight[0] -= 360;
+//    }
+    
+     jQuery(tllat).attr('value', topLeft[1]);
+     jQuery(tllon).attr('value', topLeft[0]);
+     jQuery(brlat).attr('value', bottomRight[1]);
+     jQuery(brlon).attr('value', bottomRight[0]);
+   
+   });
 
-// recreate drawings when fields are filled
-if (tllat.value !== '' && tllon.value !== '' && brlat !== '' && brlon !== '') {
-   var points = [[[tllon.value, tllat.value],
-                [brlon.value, tllat.value],
-                [brlon.value, brlat.value],
-                [tllon.value, brlat.value],
-   ]];
+   map.addInteraction(draw);
 
-   var Square = new ol.geom.Polygon(points);
-   var SquareFeature = new ol.Feature(Square);
+   // recreate drawings when fields are filled
+   if (tllat.value !== '' && tllon.value !== '' && brlat !== '' && brlon !== '') {
+      var topLeft = [Number(tllon.value), Number(tllat.value)];
+      var bottomRight = [Number(brlon.value), Number(brlat.value)];
+      if (bottomRight[0] < topLeft[0]){
+         topLeft[0] -= 360;
+      } 
 
-   drawingSource.addFeature(SquareFeature);
-//Fit to extent of features
-   map.getView().fit(map.getLayers().getArray()[1].getSource().getExtent());
-   map.getView().setZoom(map.getView().getZoom() - 1);
+      var points = [[
+         ol.proj.transform(topLeft, 'EPSG:4326', proj),
+         ol.proj.transform([bottomRight[0], topLeft[1]], 'EPSG:4326', proj),
+         ol.proj.transform(bottomRight, 'EPSG:4326', proj),
+         ol.proj.transform([topLeft[0], bottomRight[1]], 'EPSG:4326', proj),
+      ]];
+
+      var Square = new ol.geom.Polygon(points);
+      var SquareFeature = new ol.Feature(Square);
+
+      drawingSource.addFeature(SquareFeature);
+      //Fit to extent of features
+      if (ol.extent.containsExtent(map.getView().getProjection().getExtent(), map.getLayers().getArray()[1].getSource().getExtent())){
+         map.getView().fit(map.getLayers().getArray()[1].getSource().getExtent());
+         map.getView().setZoom(map.getView().getZoom() - 1);
+      }
+   }
 
 }
 
-   map.on('change:view', function () {
-       //outerThis.clear();
-       jQuery(tllat).attr('value', '');
-       jQuery(tllon).attr('value', '');
-       jQuery(brlat).attr('value', '');
-       jQuery(brlon).attr('value', '');
-   });
+build_draw(init_proj);
 
+
+function addExtraLayers(proj){
+
+   document.getElementById("droplayers").style.display = "none";
+
+   if (proj == 'EPSG:4326' || proj == 'EPSG:32661'){
+      $('#droplayers').appendTo(
+         $('.ol-overlaycontainer')
+      );
+      layer['europaveg']  = new ol.layer.Tile({
+         title: 'europaveg',
+         source: new ol.source.TileWMS({
+             url: 'https://openwms.statkart.no/skwms1/wms.vegnett?',
+             params: {'LAYERS': 'europaveg', 'TRANSPARENT':'true', 'VERSION':'1.3.0','FORMAT':'image/png', 'CRS':proj},
+             crossOrigin: 'anonymous'
+         })
+      });
+ 
+ 
+      layer['fylkesveg']  = new ol.layer.Tile({
+         title: 'fylkesveg',
+         source: new ol.source.TileWMS({
+             url: 'https://openwms.statkart.no/skwms1/wms.vegnett?',
+             params: {'LAYERS': 'fylkesveg', 'TRANSPARENT':'true', 'VERSION':'1.3.0','FORMAT':'image/png', 'CRS':proj},
+             crossOrigin: 'anonymous'
+         })
+      });
+
+      layer['riksveg']  = new ol.layer.Tile({
+         title: 'riksveg',
+         displayInLayerSwitcher: true,
+         source: new ol.source.TileWMS({
+             url: 'https://openwms.statkart.no/skwms1/wms.vegnett?',
+             params: {'LAYERS': 'riksveg', 'TRANSPARENT':'true', 'VERSION':'1.3.0','FORMAT':'image/png', 'CRS':proj},
+             crossOrigin: 'anonymous'
+         })
+      });
+
+
+     ald = document.getElementById("lrslist").children; //list of li
+     for (var i = ald.length; i--;) {
+        ald[i].onclick = function select_extralayer() {
+           //console.log(this.children[0].value);
+           selectedLayer = this.children[0].value;
+           map.addLayer(layer[selectedLayer]);
+      }}
+ 
+      document.getElementById("droplayers").style.display = "inline";
+   }
+}
+
+addExtraLayers(init_proj);
