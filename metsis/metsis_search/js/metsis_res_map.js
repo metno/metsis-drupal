b0f3d3f8b166785536d7af3e8bf43cfdf3b26af5
magnarem <magnarem@met.no>
2021-05-12 15:46:02 +0200

    Updated metadata details

--- metsis/metsis_search/js/metsis_res_map.js
+++ metsis/metsis_search/js/metsis_res_map.js
@@ -269,7 +269,8 @@ console.log("Start of metsis search map script:");
         //console.log(proj4);
         // two projections will be possible
         // 32661
-        proj4.defs('EPSG:32661', '+proj=stere +lat_0=90 +lat_ts=90 +lon_0=0 +k=0.994 +x_0=2000000 +y_0=2000000 +datum=WGS84 +units=m +no_defs');
+        //proj4.defs('EPSG:32661', '+proj=stere +lat_0=90 +lat_ts=90 +lon_0=0 +k=0.994 +x_0=2000000 +y_0=2000000 +datum=WGS84 +units=m +no_defs');
+        proj4.defs('EPSG:32661', '+proj=stere +lat_0=90 +lat_ts=90 +lon_0=0 +k=0.994 +x_0=2000000 +y_0=2000000 +ellps=WGS84 +datum=WGS84 +units=m +no_defs ');
         ol.proj.proj4.register(proj4);
         var ext32661 = [-6e+06, -3e+06, 9e+06, 6e+06];
         var center32661 = [0, 80];
