5f63b3d8f2913366c80a26820c513c8806a94498
Magnar Eivind Martinsen <magnar.eivind.martinsen@met.no>
2025-04-07 14:36:43 +0200

    Updated update function and wms-viewer

--- metsis/metsis_wms/js/metsis_wms.js
+++ metsis/metsis_wms/js/metsis_wms.js
@@ -157,9 +157,7 @@ console.log("Start of wms map script:");
          * Define the proj4 map_projections
          */
         proj4.defs("EPSG:32661", "+proj=stere +lat_0=90 +lon_0=0 +k=0.994 +x_0=2000000 +y_0=2000000 +datum=WGS84 +units=m +no_defs +type=crs");
-        //proj4.defs('EPSG:32661', '+proj=stere +lat_0=90 +lat_ts=90 +lon_0=0 +k=0.994 +x_0=2000000 +y_0=2000000 +ellps=WGS84 +datum=WGS84 +units=m +no_defs');
         proj4.defs("EPSG:32761", "+proj=stere +lat_0=-90 +lon_0=0 +k=0.994 +x_0=2000000 +y_0=2000000 +datum=WGS84 +units=m +no_defs +type=crs");
-        // proj4.defs('EPSG:32761', '+proj=stere +lat_0=-90 +lat_ts=-90 +lon_0=0 +k=0.994 +x_0=2000000 +y_0=2000000 +ellps=WGS84 +datum=WGS84 +units=m +no_defs');
         proj4.defs("EPSG:5041", "+proj=stere +lat_0=90 +lon_0=0 +k=0.994 +x_0=2000000 +y_0=2000000 +datum=WGS84 +units=m +no_defs +type=crs");
         proj4.defs("EPSG:4326", "+proj=longlat +datum=WGS84 +no_defs +type=crs");
         ol.proj.proj4.register(proj4);
